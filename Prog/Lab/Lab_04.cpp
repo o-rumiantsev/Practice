6d674abce6a050ba07c75964265146a9f1d25020
mille-nium <dianaboloten.db@gmail.com>
2018-05-01 20:54:49 +0300

    Implement unarchivation, solved #6

--- Prog/Lab/Lab_04.cpp
+++ Prog/Lab/Lab_04.cpp
@@ -1,12 +1,8 @@
 #include <iostream>
 #include <string>
-#include "structs.h"
 #define is_operator(c) (c == '+' || c == '-' || c == '/' || c == '*')
-#define has_first_priority(o) (o == "*" || o == "/")
-#define has_second_priority(o) (o == "+" || o == "-")
 
 using namespace std;
-
 
 string *split_by_operators(string line, int *split_length) {
   int operators = 0;
@@ -90,99 +86,19 @@ string *get_expression(int argc, char **argv, int *expr_length) {
   return expr;
 }
 
-auto eval(string op, auto a, auto b) {
-  if (op == "+") return a + b;
-  else if (op == "-") return a - b;
-  else if (op == "*") return a * b;
-  else return a / b;
-}
-
-auto evaluate(string *expression, int length) {
-  deque expr;
-  deque params;
-  deque operators;
-
+int evaluate(string *expression, int length) {
+  cout << "[";
   for (int i = 0; i < length; ++i) {
-    expr.push(expression[i]);
+    cout << expression[i] << " ";
   }
+  cout << "]" << endl;
 
-  while (expr.length()) {
-    string cur = expr.shift();
-
-    if (has_first_priority(cur)) {
-      if (!operators.length()) {
-        operators.push(cur);
-        continue;
-      }
-
-      string prev_op = operators.pop();
-
-      if (params.length() >= 2 && has_first_priority(prev_op)) {
-        string par2 = params.pop();
-        string par1 = params.pop();
-        auto res = eval(prev_op, float(stod(par1)), float(stod(par2)));
-        params.push(to_string(res));
-        operators.push(cur);
-      } else {
-        operators.push(prev_op);
-        operators.push(cur);
-      }
-    } else if (has_second_priority(cur)) {
-      if (!operators.length()) {
-        operators.push(cur);
-        continue;
-      }
-
-      string prev_op = operators.pop();
-
-      if ((params.length() >= 2 && has_first_priority(prev_op)) ||
-        (params.length() >= 2 && has_second_priority(prev_op))) {
-        string par2 = params.pop();
-        string par1 = params.pop();
-        auto res = eval(prev_op, float(stod(par1)), float(stod(par2)));
-        params.push(to_string(res));
-        operators.push(cur);
-      } else {
-        operators.push(prev_op);
-        operators.push(cur);
-      }
-    } else {
-      float number = float(stod(cur));
-      params.push(to_string(number));
-
-      if (expr.length() == 0) {
-        string op = operators.pop();
-
-        if (!has_first_priority(op)) {
-          operators.push(op);
-          continue;
-        }
-
-        string par2 = params.pop();
-        string par1 = params.pop();
-        auto res = eval(op, float(stod(par1)), float(stod(par2)));
-        params.push(to_string(res));
-        continue;
-      }
-
-    }
-  }
-
-  while (operators.length() != 0) {
-    string op = operators.shift();
-    string par1 = params.shift();
-    string par2 = params.shift();
-    auto res = eval(op, float(stod(par1)), float(stod(par2)));
-    params.unshift(to_string(res));
-  }
-
-  auto result = float(stod(params.pop()));
-  return result;
+  return length;
 }
 
 int main(int argc, char **argv) {
   int expr_length;
   string *expression = get_expression(argc, argv, &expr_length);
-  auto result = evaluate(expression, expr_length);
-  cout << "Result: " << result << endl;
+  int result = evaluate(expression, expr_length);
+  // cout << result << endl;
 }
--- Prog/Lab/archieve.h
+++ Prog/Lab/archieve.h
@@ -2,15 +2,11 @@
 #include <fstream>
 #include <string>
 #include <map>
+#include <math.h>
 #ifndef archieve_h
 #define archieve_h
 
-void rle(
-  std::ifstream *,
-  std::ofstream *,
-  std::filebuf *,
-  int *
-);
+#define filled_byte(bits, mask) (log2(bits) / (mask * 8) == 1 && bits != 1)
 
 void lz78(
   std::ifstream *,
@@ -20,33 +16,26 @@ void lz78(
   int *
 );
 
+void _lz78(
+  std::ifstream *,
+  std::ofstream *,
+  std::filebuf *,
+  int *,
+  int *
+);
 
 // Compression
 //
 void compress_file(char *filename, char *output_filename) {
   std::ifstream input (filename, std::ifstream::binary);
-  std::ofstream temp_out ("temp.comp", std::ios::binary);
-  temp_out << std::hex << std::uppercase;
+  std::ofstream output (output_filename, std::ios::app);
 
-  std::filebuf *buf_temp = input.rdbuf();
+  std::filebuf *buf = input.rdbuf();
 
   int input_size = 0;
-
-  rle(&input, &temp_out, buf_temp, &input_size);
-
-  input.close();
-  temp_out.close();
-
-
-  std::ifstream temp_in ("temp.comp", std::ifstream::binary);
-  std::ofstream output (output_filename, std::ios::app);
-  output << std::hex << std::uppercase;
-
-  std::filebuf *buf = temp_in.rdbuf();
-
   int output_size = 0;
 
-  lz78(&temp_in, &output, buf, &input_size, &output_size);
+  lz78(&input, &output, buf, &input_size, &output_size);
 
   int diff = input_size - output_size;
   float coef = float(float(diff) / float(input_size));
@@ -54,46 +43,8 @@ void compress_file(char *filename, char *output_filename) {
   std::cout << "Output: " << output_size << std::endl;
   std::cout << "Compression coeficient: " << coef << std::endl;
 
-  temp_in.close();
-  remove("temp.comp");
+  input.close();
   output.close();
-}
-
-
-void rle(
-  std::ifstream *input,
-  std::ofstream *output,
-  std::filebuf *buf,
-  int *input_size
-) {
-  int size = buf->pubseekoff(0, input->end, input->in);
-  *input_size = size;
-
-  int counter = 0;
-  int previous_byte = -1;
-
-  for (int i = 0; i < size; ++i) {
-    buf->pubseekpos(i, input->in);
-    int byte = buf->sgetc();
-
-    if (byte == previous_byte) {
-      --counter;
-    } else if (counter != 0) {
-      output->write(
-        (char *) &counter, sizeof(counter) / sizeof(int)
-      );
-      output->write(
-        (char *) &previous_byte, sizeof(previous_byte) / sizeof(int)
-      );
-      counter = 0;
-    } else if (previous_byte != -1) {
-      output->write(
-        (char *) &previous_byte, sizeof(previous_byte) / sizeof(int)
-      );
-    }
-
-    previous_byte = byte;
-  }
 }
 
 
@@ -105,7 +56,9 @@ void lz78(
   int *output_size
 ) {
   int size = buf->pubseekoff(0, input->end, input->in);
-  int code = -1;
+  *input_size = size;
+  int code = 1;
+  int byte_mask = 1;
   std::map<std::string, int> phrases;
   std::string phrase;
 
@@ -116,43 +69,105 @@ void lz78(
 
     std::map<std::string, int>::iterator iter = phrases.find(phrase);
     if (iter == phrases.end()) {
-      std::string encoded;
       std::string subphrase = phrase.substr(0, phrase.length() - 1);
       std::map<std::string, int>::iterator it = phrases.find(subphrase);
 
-      if (it != phrases.end()) {
-        encoded.push_back(it->second);
+      if (filled_byte(phrases.size(), byte_mask)) {
+        ++byte_mask;
       }
 
-      encoded.push_back(byte);
+      output->write(reinterpret_cast<const char *>(&it->second), byte_mask);
+      output->write((char *) &byte, sizeof(char));
+      *output_size += sizeof(char) + byte_mask;
 
-      *output << encoded;
-      *output_size += encoded.length();
-
-      phrases.insert(std::pair<std::string, int>(phrase, code--));
+      phrases.insert(std::pair<std::string, int>(phrase, code++));
       phrase.clear();
     }
   }
 }
 
 
-
 // Decompression
 //
 void decompress_file(char *filename) {
   std::ifstream input (filename, std::ifstream::binary);
+  std::ofstream output ("example.txt", std::ios::app);
+
   std::filebuf *buf = input.rdbuf();
 
-  int size = buf->pubseekoff(0, input.end, input.in);
+  int input_size = 0;
+  int output_size = 0;
 
-  for (int i = 0; i < size; ++i) {
-    buf->pubseekpos(i, input.in);
-    char *buffer = new char[1];
-    buf->sgetn(buffer, 1);
-    delete[] buffer;
-  }
+  _lz78(&input, &output, buf, &input_size, &output_size);
+
+  std::cout << "Input: " << input_size << std::endl;
+  std::cout << "Output: " << output_size << std::endl;
 
   input.close();
+  output.close();
 }
+
+int bytes_to_int(char *array_of_chars, int size) {
+  int res = 0;
+  int *bytes = new int[size];
+
+  for (int i = 0; i < size; i++) {
+    char byte = array_of_chars[i];
+    if (byte < 0) {
+      int diff = 256 + byte;
+      bytes[i] = diff;
+    } else {
+      bytes[i] = byte;
+    }
+
+  }
+
+  while (size--) res = res * 256 + bytes[size];
+  delete[] bytes;
+
+  return res;
+};
+
+void _lz78(
+  std::ifstream *input,
+  std::ofstream *output,
+  std::filebuf *buf,
+  int *input_size,
+  int *output_size
+) {
+  int size = buf->pubseekoff(0, input->end, input->in);
+  *input_size = size;
+  int code = 1;
+  int byte_mask = 1;
+  std::map<int, std::string> phrases;
+  std::string phrase;
+
+  for (int i = 0; i < size; i += byte_mask + 1) {
+    if (filled_byte(phrases.size(), byte_mask)) {
+      ++byte_mask;
+    }
+
+    buf->pubseekpos(i, input->in);
+    char *index_bytes = new char[byte_mask];
+    buf->sgetn(index_bytes, byte_mask);
+    int index = bytes_to_int(index_bytes, byte_mask);
+    buf->pubseekpos(i + byte_mask, input->in);
+    char byte = buf->sgetc();
+
+    if (index != 0) {
+      std::map<int, std::string>::iterator it = phrases.find(index);
+      *output << it->second;
+      phrase = it->second;
+    }
+
+    output->write((char *) &byte, sizeof(char));
+    phrase.push_back(byte);
+    *output_size += phrase.length();
+    phrases.insert(std::pair<int, std::string>(code++, phrase));
+    phrase.clear();
+    delete[] index_bytes;
+  }
+}
+
 
 #endif
